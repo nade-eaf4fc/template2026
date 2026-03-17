82a78bf8869703147617670c523c13f807188b8d
nade-eaf4fc <nade-eaf4fc@users.noreply.github.com>
2026-03-17 15:17:01 +0900

    add template

--- scripts/main.jl
+++ scripts/main.jl
@@ -1,6 +1,7 @@
 using Distributed
 const SCRIPTDIR = @__DIR__
 const SRCDIR = normpath(joinpath(SCRIPTDIR, "..", "src"))
+
 @everywhere begin
 	using FFTW
 	using DataFrames
@@ -10,35 +11,37 @@ const SRCDIR = normpath(joinpath(SCRIPTDIR, "..", "src"))
 	using Dates
 	using Random
 	using Base.Threads
-	const FFT_PLANS = Dict{Int, FFTW.cFFTWPlan{ComplexF64}}()
+
+	const FFT_PLANS = Dict{Int, Any}()
+
 	function get_fft_plan(N::Int)
 		return get!(FFT_PLANS, N) do
 			buf = Vector{ComplexF64}(undef, N)
 			plan_fft!(buf, flags = FFTW.MEASURE)
 		end
 	end
-
-
 end
-
 
 function main()
 	datestr = Dates.format(Dates.now(), "yyyymmdd_HHMMSS")
 	prefix = "hogehoge"
 	projdir = normpath(joinpath(@__DIR__, ".."))
-	datadir = joinpath(projdir, "data")
-	mkpath(datadir)
-	outdir = joinpath(datadir, "out_$(datestr)_$(prefix)")
+
+	resultsdir = joinpath(projdir, "results")
+	mkpath(resultsdir)
+
+	outdir = joinpath(resultsdir, "out_$(prefix)_$(datestr)")
 	mkpath(outdir)
+
 	@info "outdir = $outdir"
 	@everywhere Random.seed!(0xC0FFEE + myid())
 
+	df = DataFrame(a = [1], b = [2])
 
-
-	df = 1, 2
 	ts = Dates.format(Dates.now(), "yyyymmdd_HHMMSS")
-	path = joinpath(outdir, "ser_$(case_name)_fsf_$(st_fsf.fsf)_npay_$(npay)_iter_$(iter)_pid_$(myid())_$(ts).csv")
+	path = joinpath(outdir, "sample_$(ts).csv")
 	CSV.write(path, df)
+
 	return nothing
 end
 
